bc3df7f9704ca30160fbd3f836c95d7d429bcae9
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-04-11 15:57:37 +0200

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by Eylul Naz Can on 11.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
