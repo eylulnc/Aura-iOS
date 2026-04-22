d684cd89d156d3ca27582a60e11c7eb50c485b5c
Eylül Naz Can <65118253+eylulnc@users.noreply.github.com>
2026-04-22 11:39:08 +0200

    Update AuraApp.swift

--- Aura/AuraApp.swift
+++ Aura/AuraApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import SwiftData
+import FirebaseCore
 
 @main
 struct AuraApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: MoodEntry.self)
     }
 }
